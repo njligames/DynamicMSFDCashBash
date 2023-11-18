e2457f51f0cb4d8a9e392de2346ba021a2f41252
James Folk <jamesfolk1@gmail.com>
2023-11-17 22:08:09 -0500

    updated the port

--- app.py
+++ app.py
@@ -150,5 +150,5 @@ def index():
     return render_template("index.html", len = len(getTicketBlocks()), tickets = getTicketBlocks())
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=4242)
 
