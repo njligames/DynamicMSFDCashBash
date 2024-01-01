5a13d959f293fa1d430a2d1111147e81c05213e1
James Folk <jamesfolk1@gmail.com>
2024-01-01 15:42:59 -0500

    the modals are now dynamically generated

--- app.py
+++ app.py
@@ -99,6 +99,14 @@ def generateExampleTicketArrayMap():
         ticket_map_array.append(ticket)
     return ticket_map_array
 
+def generateExampleModalArrayMap():
+    modal_map_array = []
+    for i in range(1, 501):
+        cart_value='VSAPQMG6VEPXY'
+        ticket = {'cart_value':cart_value, 'ticket_number':i}
+        modal_map_array.append(ticket)
+    return modal_map_array
+
 def loadTicketArrayMap():
     tickets = Tickets()
 
@@ -150,6 +158,139 @@ def getTicketBlocks():
 
     return ticket_array
 
+
+def getModalBlocks():
+    modal_map_array = generateExampleModalArrayMap()
+
+    def getModalBlock(ticket_number, cart_value):
+        code_txt = """
+        <div
+          class="modal fade bd-example-modal-lg"
+          id="exampleModal{ticket_number}"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Cash Bash Ticket</h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body" id="exampleModalBody">
+                <h1 style="text-align: center"><strong>1st Prize: $10,000</strong></h1>
+                <h1 style="text-align: center"><strong>2nd Prize: $2,500</strong></h1>
+                <h1 style="text-align: center"><strong>13 Prizes: $500.00</strong></h1>
+                <h1 style="text-align: center"><strong>10 Prizes: $100.00</strong></h1>
+                <div>&nbsp;</div>
+                <ol>
+                  <li>$100.00 Per Ticket. Winner need not be present.</li>
+                  <li>
+                    All numbers will be drawn on September 23rd, 2023 at the Mt. Sinai
+                    Fire Department, Station 1, 133 Mount Sinai Avenue.
+                  </li>
+                  <li>
+                    Gates will open at 1 pm and event will run until 5 pm, rain or
+                    shine.
+                  </li>
+                  <li>All winning numbers are re-entered for additional prizes.</li>
+                  <li>
+                    Only 2 ADULTS per ticket will be allowed entry. Please bring your
+                    own lawn chair.
+                  </li>
+                  <li>
+                    Only (up to) 500 tickets will be sold. Tickets must be shown at the
+                    gate for entry.
+                  </li>
+                  <li>No one under 21 will be admited. NO EXCEPTIONS!!!</li>
+                  <li>
+                    <strong>All tickets will be mailed to the address you put on the
+                      application once your payment clears. Tickets purchased after
+                      9/1/2023 can be picked up at the gate with photo ID.</strong
+                    >
+                  </li>
+                  <li>
+                    You can choose your personal number 1-500. If a ticket number choice
+                    is not available, a random number will then be sent to you in its
+                    place.
+                  </li>
+                  <li>
+                    For additional questions contact: Cash Bash Committee at:
+                    msfdcashbash@gmail.com
+                  </li>
+                  <li>
+                    Entertainment, BBQ and drinks will be available during drawing, all
+                    included with ticket purchase.
+                  </li>
+                  <li>
+                    IF LESS THAN 500 TICKETS ARE SOLD, ALL PRIZES WILL BE PRORATED AT 60% OF TOTAL SOLD
+                  </li>
+                </ol>
+                <p>
+                  <strong>
+                  Enter for a chance to win an additional free ticket for five more dollars, select in drop down.
+                    </strong
+                  >
+                </p>
+              </div>
+              <div class="col-xs-1" align="center">
+
+
+                <!-- TODO: edit so that it uses paypal api -->
+                <!-- start button for a ticket -->
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_self">
+                   <input type="hidden" name="cmd" value="_s-xclick">
+                   <input type="hidden" name="hosted_button_id" value="{cart_value}">
+                   <table>
+                      <tr>
+                         <td><input type="hidden" name="on0" value="Cash Bash Ticket #{ticket_number}">Cash Bash Ticket #{ticket_number}</td>
+                      </tr>
+                      <tr>
+                         <td>
+                            <select name="os0">
+                               <option value="Ticket">Ticket $100.00 USD</option>
+                               <option value="Ticket & Chance">Ticket & Chance $105.00 USD</option>
+                            </select>
+                         </td>
+                      </tr>
+                      <tr>
+                         <td><input type="hidden" name="on1" value="Winner Contact #">Winner Contact #</td>
+                      </tr>
+                      <tr>
+                         <td><input type="text" name="os1" maxlength="200"></td>
+                      </tr>
+                   </table>
+                   <input type="hidden" name="currency_code" value="USD">
+                   <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                   <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                </form>
+                <!-- end button for a ticket -->
+
+              </div>
+              <div class="modal-footer"></div>
+            </div>
+          </div>
+        </div>
+
+        """
+
+        return code_txt.format(ticket_number = ticket_number, cart_value = cart_value)
+
+    modal_array = []
+
+    for modal in modal_map_array:
+        modal_array.append(getModalBlock(modal['ticket_number'], modal['cart_value']))
+
+    return modal_array
+
 @app.route("/hello")
 def hello_world():
     return jsonify("hello, world!")
@@ -173,7 +314,7 @@ def success():
 
 @app.route("/")
 def index():
-    return render_template("index.html", len = len(getTicketBlocks()), tickets = getTicketBlocks())
+    return render_template("index.html", ticket_len = len(getTicketBlocks()), tickets = getTicketBlocks(), modal_len = len(getModalBlocks()), modals = getModalBlocks())
 
 if __name__ == "__main__":
     app.run(port=4242)
